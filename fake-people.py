e1a8f29ad3720b98f3e00e179b2afec3e706c0db
Jana <yanix2x2@gmail.com>
2025-01-21 13:27:44 +0300

    fix format issues

--- fake-people.py
+++ fake-people.py
@@ -7,32 +7,76 @@ import file_operations
 
 
 LETTER_MAPPING = {
-    'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
-    'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
-    'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
-    'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
-    'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
-    'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
-    'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
-    'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
-    'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
-    'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
-    'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
-    'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
-    'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
-    'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
-    'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
-    'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
-    'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
-    'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
-    'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
-    'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
-    'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
-    'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+    'а': 'а͠', 
+    'б': 'б̋', 
+    'в': 'в͒͠',
+    'г': 'г͒͠', 
+    'д': 'д̋', 
+    'е': 'е͠',
+    'ё': 'ё͒͠', 
+    'ж': 'ж͒', 
+    'з': 'з̋̋͠',
+    'и': 'и', 
+    'й': 'й͒͠', 
+    'к': 'к̋̋',
+    'л': 'л̋͠', 
+    'м': 'м͒͠', 
+    'н': 'н͒',
+    'о': 'о̋', 
+    'п': 'п̋͠', 
+    'р': 'р̋͠',
+    'с': 'с͒', 
+    'т': 'т͒', 
+    'у': 'у͒͠',
+    'ф': 'ф̋̋͠', 
+    'х': 'х͒͠', 
+    'ц': 'ц̋',
+    'ч': 'ч̋͠', 
+    'ш': 'ш͒͠', 
+    'щ': 'щ̋',
+    'ъ': 'ъ̋͠', 
+    'ы': 'ы̋͠', 
+    'ь': 'ь̋',
+    'э': 'э͒͠͠', 
+    'ю': 'ю̋͠', 
+    'я': 'я̋',
+    'А': 'А͠', 
+    'Б': 'Б̋', 
+    'В': 'В͒͠',
+    'Г': 'Г͒͠', 
+    'Д': 'Д̋', 
+    'Е': 'Е',
+    'Ё': 'Ё͒͠', 
+    'Ж': 'Ж͒', 
+    'З': 'З̋̋͠',
+    'И': 'И', 
+    'Й': 'Й͒͠', 
+    'К': 'К̋̋',
+    'Л': 'Л̋͠', 
+    'М': 'М͒͠', 
+    'Н': 'Н͒',
+    'О': 'О̋', 
+    'П': 'П̋͠', 
+    'Р': 'Р̋͠',
+    'С': 'С͒', 
+    'Т': 'Т͒', 
+    'У': 'У͒͠',
+    'Ф': 'Ф̋̋͠', 
+    'Х': 'Х͒͠', 
+    'Ц': 'Ц̋',
+    'Ч': 'Ч̋͠', 
+    'Ш': 'Ш͒͠', 
+    'Щ': 'Щ̋',
+    'Ъ': 'Ъ̋͠', 
+    'Ы': 'Ы̋͠', 
+    'Ь': 'Ь̋',
+    'Э': 'Э͒͠͠', 
+    'Ю': 'Ю̋͠', 
+    'Я': 'Я̋',
     ' ': ' '
 }
 
-skills = [
+SKILLS = [
     'Стремительный прыжок',
     'Электрический выстрел',
     'Ледяной удар',
@@ -43,44 +87,43 @@ skills = [
     'Огненный заряд'
 ]
 
-os.makedirs('cards', exist_ok=True)
-fake = Faker("ru_RU")
 
-
-def get_cards():
+def get_cards(fake):
     for i in range(10):
-        sample_skills = sample(skills, 3)
-        ruined_sample_skills = []
+        sample_SKILLS = sample(SKILLS, 3)
+        ruined_sample_SKILLS = []
 
-        for skill in sample_skills:
+        for skill in sample_SKILLS:
             changed_letters = []
             for letter in skill:
                 letter = LETTER_MAPPING[letter]
                 changed_letters.append(letter)
             changed_skill = ''.join(changed_letters)
-            ruined_sample_skills.append(changed_skill)
+            ruined_sample_SKILLS.append(changed_skill)
 
         context = {
-            "first_name": fake.first_name(),
-            "last_name": fake.last_name(),
-            "job": fake.job(),
-            "town": fake.city(),
-            "strength": randint(3, 18),
-            "agility": randint(3, 18),
-            "endurance": randint(3, 18),
-            "intelligence": randint(3, 18),
-            "luck": randint(3, 18),
-            "skill_1": ruined_sample_skills[0],
-            "skill_2": ruined_sample_skills[1],
-            "skill_3": ruined_sample_skills[2]
+            'first_name': fake.first_name(),
+            'last_name': fake.last_name(),
+            'job': fake.job(),
+            'town': fake.city(),
+            'strength': randint(3, 18),
+            'agility': randint(3, 18),
+            'endurance': randint(3, 18),
+            'intelligence': randint(3, 18),
+            'luck': randint(3, 18),
+            'skill_1': ruined_sample_SKILLS[0],
+            'skill_2': ruined_sample_SKILLS[1],
+            'skill_3': ruined_sample_SKILLS[2]
         }
 
         file_operations.render_template(
-            "charsheet.svg", 
-            f"cards/result-{i}.svg", 
+            'charsheet.svg', 
+            f'cards/result-{i}.svg', 
             context
         )
 
 
 if __name__ == '__main__':
-    get_cards()
+    fake = Faker('ru_RU')
+    os.makedirs('cards', exist_ok=True)
+    get_cards(fake)
